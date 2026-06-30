74649c3b29688b64f7f38a2fe8a45a4f64f33774
Marcos Ledo Martinez <207266931+MarcosLedoMartinez@users.noreply.github.com>
2026-06-30 23:16:59 +0100

    Primer commit

---
+++ src/main/java/com/marcosledo/vass/springboot_introduction/SpringbootIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.marcosledo.vass.springboot_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosledo/vass/springboot_introduction/SpringbootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosledo.vass.springboot_introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
